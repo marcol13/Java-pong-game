d5dcfb6cea4d8f947aa6888a3b0bbe7971d39570
Marcin <marcinkrueger@onet.eu>
2021-01-05 17:20:45 +0100

    fix menu bug

--- src/GameData.java
+++ src/GameData.java
@@ -1,9 +1,11 @@
 public class GameData {
-    public static int windowWeight = 1280;
-    public static int windowHeight = 720;
-    public static String title = "Pong pong pong pong";
-    public static int fontSize = (int)((windowWeight*windowWeight)/windowHeight*0.01875);
-    public static int menuRectTitleH = (int)(windowHeight * 0.175);
-    public static int menuRectSubtitleH = (int)(windowHeight * 0.075);
-    public static int menuRectOptionH = windowHeight - menuRectTitleH - menuRectSubtitleH;
+    public static int windowWidth = 800;
+    public static int windowHeight = 600;
+    public static final String title = "Pong pong pong pong";
+    public static int fontSize = (int)((windowWidth * windowWidth) / windowHeight * 0.01875);
+    public static int menuPaddingW = 20;
+    public static int menuPaddingH = 20;
+    public static int menuRectTitleH = (int)((windowHeight - (menuPaddingH * 2)) * 0.175);
+    public static int menuRectSubtitleH = (int)((windowHeight - (menuPaddingH * 2)) * 0.075);
+    public static int menuRectOptionH = windowHeight - (menuPaddingH * 2) - menuRectTitleH - menuRectSubtitleH;
 }
--- src/Window.java
+++ src/Window.java
@@ -12,13 +12,13 @@ public class Window extends JFrame implements Runnable{
     Font fontTitle = new Font("TimesRoman", Font.PLAIN, (int)(GameData.fontSize * 1.75));
     Font fontSubtitle = new Font("TimesRoman", Font.PLAIN, (int)(GameData.fontSize * 0.75));
     Font fontOption = new Font("TimesRoman", Font.PLAIN, GameData.fontSize);
-    Rectangle menuRectTitle = new Rectangle(0,0, GameData.windowWeight, GameData.menuRectTitleH);
-    Rectangle menuRectSubtitle = new Rectangle(0,GameData.menuRectTitleH, GameData.windowWeight, GameData.menuRectSubtitleH);
-    Rectangle menuRectOptions = new Rectangle(0, GameData.menuRectTitleH + GameData.menuRectSubtitleH, GameData.windowWeight, GameData.menuRectOptionH);
+    Rectangle menuRectTitle = new Rectangle(GameData.menuPaddingW,GameData.menuPaddingH, GameData.windowWidth - GameData.menuPaddingW * 2, GameData.menuRectTitleH);
+    Rectangle menuRectSubtitle = new Rectangle(GameData.menuPaddingW,GameData.menuRectTitleH + GameData.menuPaddingH, GameData.windowWidth - GameData.menuPaddingW * 2, GameData.menuRectSubtitleH);
+    Rectangle menuRectOptions = new Rectangle(GameData.menuPaddingW, GameData.menuRectTitleH + GameData.menuRectSubtitleH + GameData.menuPaddingH, GameData.windowWidth - GameData.menuPaddingW * 2, GameData.menuRectOptionH);
 
     public Window(){
         //Dodać fullscreen
-        this.setSize(GameData.windowWeight,GameData.windowHeight);
+        this.setSize(GameData.windowWidth,GameData.windowHeight);
         this.setTitle(GameData.title);
         this.setResizable(false);
         this.setVisible(true);
@@ -44,7 +44,7 @@ public class Window extends JFrame implements Runnable{
         int tileSize = rect.height / arrLen;
         for(int i = 0; i < arrLen; i++) {
             int x = rect.x + (rect.width - metrics.stringWidth(text[i])) / 2;
-            int y = rect.y + ((tileSize - metrics.getHeight()) / 2) + metrics.getAscent() + (i+1) * tileSize;
+            int y = rect.y + ((tileSize - metrics.getHeight()) / 2) + metrics.getAscent() + i * tileSize;
             g.setFont(font);
             g.drawString(text[i], x, y);
         }
@@ -52,7 +52,7 @@ public class Window extends JFrame implements Runnable{
 
     public void update(double dt){
         g.setColor(Color.BLACK);
-        g.fillRect(0,0,GameData.windowWeight, GameData.windowHeight);
+        g.fillRect(0,0,GameData.windowWidth, GameData.windowHeight);
 
         g.setColor(Color.RED);
         drawCenteredString(g, menu.title, menuRectTitle, fontTitle);
@@ -69,8 +69,7 @@ public class Window extends JFrame implements Runnable{
 
     public void run(){
         double lastFrame = 0.0;
-        update(0.0);
-        /*while(true){
+        while(true){
             double time = Time.getTime();
             double deltaTime = time - lastFrame;
             lastFrame = time;
@@ -81,6 +80,6 @@ public class Window extends JFrame implements Runnable{
                 Thread.sleep(30);
             }catch(Exception e){
             }
-        }*/
+        }
     }
 }
