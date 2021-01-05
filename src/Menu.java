af7e3135198a1404465212b167bcfea30b886745
Marcin <marcinkrueger@onet.eu>
2021-01-05 17:24:42 +0100

    code refactor - menu

--- src/Menu.java
+++ src/Menu.java
@@ -9,6 +9,13 @@ public class Menu {
     public static String[] titleString = {title,logged};
     public static String[] options = {"PLAY", "TOURNAMENT", "STATS", "USER", "SETTINGS", "EXIT"};
 
+    Font fontTitle = new Font("TimesRoman", Font.PLAIN, (int)(GameData.fontSize * 1.75));
+    Font fontSubtitle = new Font("TimesRoman", Font.PLAIN, (int)(GameData.fontSize * 0.75));
+    Font fontOption = new Font("TimesRoman", Font.PLAIN, GameData.fontSize);
+    Rectangle menuRectTitle = new Rectangle(GameData.menuPaddingW,GameData.menuPaddingH, GameData.windowWidth - GameData.menuPaddingW * 2, GameData.menuRectTitleH);
+    Rectangle menuRectSubtitle = new Rectangle(GameData.menuPaddingW,GameData.menuRectTitleH + GameData.menuPaddingH, GameData.windowWidth - GameData.menuPaddingW * 2, GameData.menuRectSubtitleH);
+    Rectangle menuRectOptions = new Rectangle(GameData.menuPaddingW, GameData.menuRectTitleH + GameData.menuRectSubtitleH + GameData.menuPaddingH, GameData.windowWidth - GameData.menuPaddingW * 2, GameData.menuRectOptionH);
+
     public Menu(){
 
         /*try {
@@ -18,6 +25,32 @@ public class Menu {
         } catch (IOException |FontFormatException e) {
             System.out.println("abcd");
         }*/
+    }
+
+    public void drawCenteredString(Graphics g, String text, Rectangle rect, Font font) {
+        FontMetrics metrics = g.getFontMetrics(font);
+        int x = rect.x + (rect.width - metrics.stringWidth(text)) / 2;
+        int y = rect.y + ((rect.height - metrics.getHeight()) / 2) + metrics.getAscent();
+        g.setFont(font);
+        g.drawString(text, x, y);
+    }
+
+    public void drawCenteredArrStrings(Graphics g, String[] text, Rectangle rect, Font font) {
+        FontMetrics metrics = g.getFontMetrics(font);
+        int arrLen = text.length;
+        int tileSize = rect.height / arrLen;
+        for(int i = 0; i < arrLen; i++) {
+            int x = rect.x + (rect.width - metrics.stringWidth(text[i])) / 2;
+            int y = rect.y + ((tileSize - metrics.getHeight()) / 2) + metrics.getAscent() + i * tileSize;
+            g.setFont(font);
+            g.drawString(text[i], x, y);
+        }
+    }
+
+    public void drawMenu(Graphics g){
+        drawCenteredString(g, title, menuRectTitle, fontTitle);
+        drawCenteredString(g, logged, menuRectSubtitle, fontSubtitle);
+        drawCenteredArrStrings(g, options, menuRectOptions, fontOption);
     }
 
     public static void changeLogged(String user){
--- src/Window.java
+++ src/Window.java
@@ -9,12 +9,7 @@ public class Window extends JFrame implements Runnable{
     KListener kl = new KListener();
     Users user = new Users();
     Menu menu = new Menu();
-    Font fontTitle = new Font("TimesRoman", Font.PLAIN, (int)(GameData.fontSize * 1.75));
-    Font fontSubtitle = new Font("TimesRoman", Font.PLAIN, (int)(GameData.fontSize * 0.75));
-    Font fontOption = new Font("TimesRoman", Font.PLAIN, GameData.fontSize);
-    Rectangle menuRectTitle = new Rectangle(GameData.menuPaddingW,GameData.menuPaddingH, GameData.windowWidth - GameData.menuPaddingW * 2, GameData.menuRectTitleH);
-    Rectangle menuRectSubtitle = new Rectangle(GameData.menuPaddingW,GameData.menuRectTitleH + GameData.menuPaddingH, GameData.windowWidth - GameData.menuPaddingW * 2, GameData.menuRectSubtitleH);
-    Rectangle menuRectOptions = new Rectangle(GameData.menuPaddingW, GameData.menuRectTitleH + GameData.menuRectSubtitleH + GameData.menuPaddingH, GameData.windowWidth - GameData.menuPaddingW * 2, GameData.menuRectOptionH);
+
 
     public Window(){
         //Dodać fullscreen
@@ -25,39 +20,17 @@ public class Window extends JFrame implements Runnable{
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.addKeyListener(kl);
         g = (Graphics2D)this.getGraphics();
-
-        user.getCurrUser();
         menu.changeLogged(user.getCurrUser());
     }
 
-    public void drawCenteredString(Graphics g, String text, Rectangle rect, Font font) {
-        FontMetrics metrics = g.getFontMetrics(font);
-        int x = rect.x + (rect.width - metrics.stringWidth(text)) / 2;
-        int y = rect.y + ((rect.height - metrics.getHeight()) / 2) + metrics.getAscent();
-        g.setFont(font);
-        g.drawString(text, x, y);
-    }
 
-    public void drawCenteredArrStrings(Graphics g, String[] text, Rectangle rect, Font font) {
-        FontMetrics metrics = g.getFontMetrics(font);
-        int arrLen = text.length;
-        int tileSize = rect.height / arrLen;
-        for(int i = 0; i < arrLen; i++) {
-            int x = rect.x + (rect.width - metrics.stringWidth(text[i])) / 2;
-            int y = rect.y + ((tileSize - metrics.getHeight()) / 2) + metrics.getAscent() + i * tileSize;
-            g.setFont(font);
-            g.drawString(text[i], x, y);
-        }
-    }
 
     public void update(double dt){
         g.setColor(Color.BLACK);
         g.fillRect(0,0,GameData.windowWidth, GameData.windowHeight);
 
         g.setColor(Color.RED);
-        drawCenteredString(g, menu.title, menuRectTitle, fontTitle);
-        drawCenteredString(g, menu.logged, menuRectSubtitle, fontSubtitle);
-        drawCenteredArrStrings(g, menu.options, menuRectOptions, fontOption);
+        menu.drawMenu(g);
 
         if(kl.getKeyPressed(KeyEvent.VK_UP)){
             System.out.println("Strzałka w górę!");
