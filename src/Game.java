2a8b1efd24793f6735cf6dea851ae03bde27d776
Marcin <marcinkrueger@onet.eu>
2021-01-06 19:12:27 +0100

    add board

--- src/Game.java
+++ src/Game.java
@@ -8,6 +8,9 @@ public class Game {
     Ball ball;
 
     public int score[] = new int[2];
+    public int gameBoardW = GameData.windowWidth - 2 * GameData.gamePaddingW;
+    public int gameBoardH = GameData.windowHeight - GameData.gamePaddingW - GameData.gamePaddingH;
+    public Rectangle board;
     public int startWidth = 20;
     public int startHeight = 200;
     public int gamePadding = 30;
@@ -15,12 +18,14 @@ public class Game {
     public int spawnPointX = (int)((GameData.windowWidth - ballSize)/2);
     public int spawnPointY = (int)((GameData.windowHeight - ballSize) / 2);
 
+
     //1V1
     public Game(Graphics2D g, KListener kl){
         Bar player1Bar = new Bar(gamePadding,0, startWidth, startHeight, Color.WHITE);
         Bar player2Bar = new Bar(GameData.windowWidth - gamePadding - startWidth,0, startWidth, startHeight, Color.WHITE);
         player1 = new PlayerController(player1Bar, kl, KeyEvent.VK_UP, KeyEvent.VK_DOWN);
         player2 = new PlayerController(player2Bar, kl, KeyEvent.VK_W, KeyEvent.VK_S);
+        board = new Rectangle(GameData.gamePaddingW, GameData.gamePaddingH, gameBoardW, gameBoardH);
         ball = new Ball(ballSize,spawnPointX, spawnPointY, 10, 5, Color.WHITE);
         ball.drawBall(g);
         player1.bar.drawBar(g);
@@ -28,6 +33,11 @@ public class Game {
     }
 
     public void updateGame(Graphics2D g, double dt){
+        Stroke oldStroke = g.getStroke();
+        g.setStroke(new BasicStroke(2));
+        g.setColor(Color.WHITE);
+        g.draw(board);
+        g.setStroke(oldStroke);
         ball.updateBall(player1, player2);
         player1.update(dt);
         player2.update(dt);
--- src/GameData.java
+++ src/GameData.java
@@ -5,6 +5,8 @@ public class GameData {
     public static int fontSize = (int)((windowWidth * windowWidth) / windowHeight * 0.01875);
     public static int menuPaddingW = 20;
     public static int menuPaddingH = 20;
+    public static int gamePaddingW = 20;
+    public static int gamePaddingH = 150;
     public static int menuRectTitleH = (int)((windowHeight - (menuPaddingH * 2)) * 0.175);
     public static int menuRectSubtitleH = (int)((windowHeight - (menuPaddingH * 2)) * 0.075);
     public static int menuRectOptionH = windowHeight - (menuPaddingH * 2) - menuRectTitleH - menuRectSubtitleH;
